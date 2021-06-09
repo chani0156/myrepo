133a4997f4e932ed940c24db44abaf8a4bcfe6e2
User <User@DESKTOP-A3LNA5K>
2021-06-10 00:17:01 +0300

    some descriptive text

---
+++ client/src/Utils/Utils.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+
+    const getAllTasks=async()=>{
+   // let usersList= await axios.get(usersUrl)
+    return "";
+    }
+
+    const addTask=async()=>{
+        return ""
+    }
+    export default {addTask,getAllTasks}
